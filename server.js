8bd7ff5d8e7c559f62778ddcb741a0d42907cf15
astradae <astradaezequiel.tpi@gmail.com>
2022-02-21 22:01:14 -0300

    GrahQL Setup

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('Hello world');
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql')
+const app = express()
+const schema = require('./graohql/schema')
+app.get('/', (req, res) => {
+    res.send('welcome to my graphql api')
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+app.listen(3000)
+console.log('server is running on port 3000')
