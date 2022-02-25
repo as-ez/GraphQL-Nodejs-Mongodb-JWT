6bf861a8692e9d5e0b362df464b9e74bbc8da60a
astradae <astradaezequiel.tpi@gmail.com>
2022-02-25 11:20:04 -0300

    Login - Mutation

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -17,13 +17,43 @@ const register = {
         const user = new User({username, email, password, displayName})
         await user.save();
 
-        const token = createJWTToken({username, email, displayName})
+        const token = createJWTToken({
+            _id: user._id,
+            username: user.username, 
+            email: user.email
+        })
 
         console.log(token)
         return token;
     }
 }
 
+const login = {
+    type: GraphQLString,
+    args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+    },
+    async resolve(_, args) {
+
+        const user= await User.findOne({email: args.email}).select('+password')
+
+        if (!user || args.password !== user.password) throw new Error('Invalid Credentials')
+
+        const token = createJWTToken({
+            _id: user._id,
+            username: user.username, 
+            email: user.email
+        })
+
+        console.log(user)
+
+        return token
+    }
+
+}
+
 module.exports = {
     register,
+    login
 }
